ddb2aa49b9fd55b178f1734bcaafb3650be5b61c
rbeninca@gmail.com <rbeninca@gmail.com>
2025-03-13 22:12:43 -0300

    random

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -2,33 +2,39 @@ package com.ifsc.contaclick;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity
-{
-    TextView textView;
-    int contador=0;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edmin, edmax;
+    TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView=findViewById(R.id.textView);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textView.setText(Integer.toString(contador));
-                contador++;
-            }
+        edmin=findViewById(R.id.edmin);
+        edmax=findViewById(R.id.edmax);
+        tvResultado=findViewById(R.id.tvResultado);
+        Button b=findViewById(R.id.button);
+
+        b.setOnClickListener(v -> {
+            int min = Integer.parseInt(edmin.getText().toString());
+            int max = Integer.parseInt(edmax.getText().toString());
+            //Random
+            Random random=new Random();
+
+            int n=random.nextInt(max-min)+min;
+
+            tvResultado.setText(Integer.toString(n));
+
         });
-    }
-
-
-    public void onClick(View view){
 
     }
+
+
 }
