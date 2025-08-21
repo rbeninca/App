8b8e0d8d48ebe96cc26b7994ae7d21ea80775c7f
Romulo Beninca <rbeninca@gmail.com>
2025-08-20 22:28:24 -0300

    sorteio

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +12,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    EditText editTextMin,editTextMax;
+    TextView textViewResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Associar os objetos declarados e inicializados apatir do XML com variaveis locais.
+        button=findViewById(R.id.button);
+        editTextMin=findViewById(R.id.edMin);
+        editTextMax=findViewById(R.id.edMax);
+        textViewResultado=findViewById(R.id.tvResult);
+
+        button.setOnClickListener((v) -> {
+            int min=Integer.parseInt(  editTextMin.getText().toString() );
+            int max=Integer.parseInt(  editTextMax.getText().toString() );
+            int sorteado=0;
+            Random random=new Random();
+            sorteado=random.nextInt((max-min))+min;
+            sorteado= (int) (Math.random()*(max-min)+min);
+            textViewResultado.setText(Integer.toString(sorteado));
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
