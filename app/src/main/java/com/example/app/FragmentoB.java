ee769260a1861421db6b4b44ffbd1a8950aab501
aluno <rbeninca@gmail.com>
2025-09-10 21:37:15 -0300

    fragmentos

---
+++ app/src/main/java/com/example/app/FragmentoB.java
@@ -0,0 +1,36 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentoB extends Fragment {
+
+     TextView textView;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Bundle bundle= getArguments();
+        textView.setText(bundle.getString("msg"));
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_fragmento_b, container, false);
+        textView =v.findViewById(R.id.textView);
+        return v;
+    }
+}
--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
+    FrameLayout frameLayout;
+    Button buttonA, buttonB;
+    FragmentoA fragmentoA ;
+    FragmentoB fragmentoB ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        buttonA=findViewById(R.id.buttonA);
+        buttonB=findViewById(R.id.buttonB);
+        frameLayout=findViewById(R.id.frameLayout);
+
+
+
+        buttonA.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            if (fragmentoA ==null){
+                fragmentoA =new FragmentoA();
+            }
+
+            fragmentTransaction.replace(R.id.frameLayout,fragmentoA);
+            fragmentTransaction.commit();
+
         });
+        buttonB.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            if (fragmentoB==null){
+                fragmentoB=new FragmentoB();
+            }
+            Bundle bundle = new Bundle();
+            bundle.putString("msg","Olá ");
+            fragmentoB.setArguments(bundle);
+            fragmentTransaction.replace(R.id.frameLayout,fragmentoB);
+            fragmentTransaction.commit();
+        });
+
+
+
+
+
+
+
     }
 }
