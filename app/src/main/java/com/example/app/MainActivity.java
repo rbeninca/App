284a9e5e771e4774ecf9c15d8f4ef9e2b87b26e8
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2025-09-18 14:09:12 -0300

    imc

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edPeso, edAltura;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edPeso = findViewById(R.id.edpeso);
+        edAltura = findViewById(R.id.edaltura);
+        btnCalcular = findViewById(R.id.button);
+        btnCalcular.setOnClickListener(v -> {
+            Double altura = Double.parseDouble(edAltura.getText().toString());
+            Double peso = Double.parseDouble(edPeso.getText().toString());
+
+            Intent intent = new Intent(MainActivity.this, ImcResultado.class);
+            intent.putExtra("altura", altura);
+            intent.putExtra("peso", peso);
+            startActivities(intent);
+
         });
     }
 }
