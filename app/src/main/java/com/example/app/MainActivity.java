84e70d51e8f36994a7ff456058ae0e9fe1bb2d54
Romulo Beninca <rbeninca@gmail.com>
2026-04-09 12:11:04 -0300

    imc

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
+    EditText edPeso,edAltura;
+    TextView tvImc;
+    ImageView imageView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edPeso=findViewById(R.id.edPeso);
+        edAltura=findViewById(R.id.edAltura);
+        tvImc =findViewById(R.id.tvImc);
+        imageView=findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener( v -> {
+            String strPeso= edPeso.getText().toString();
+            String strAltura= edAltura.getText().toString();
+
+
+            Double peso=  Double.parseDouble(strPeso);
+            Double altura=  Double.parseDouble(strAltura);
+
+            Double imc = peso/(altura*altura);
+            DecimalFormat dc= new DecimalFormat("##.##");
+
+            tvImc.setText(dc.format(imc));
+
         });
+
     }
 }
