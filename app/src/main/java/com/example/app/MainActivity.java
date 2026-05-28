b73c3c4a67be399c81b7d14d8ee3c96ed4772a30
Your Name <rbeninca@gmail.com>
2026-05-28 20:25:59 -0300

    framentos

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,24 +1,59 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+    FragmentoA fragmentoA;
+    FragmentoB fragmentoB;
+    FragmentoC fragmentoC;
+    Button buttonA,buttonB,buttonC;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        buttonA=findViewById(R.id.buttonA);
+        buttonB=findViewById(R.id.button4);
+        buttonC=findViewById(R.id.buttonC);
+
+        //Inicialização Gerenciador de Fragmentos;
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        //Inicia uma transação com FragmentMangater
+        FragmentTransaction  transaction=fragmentManager.beginTransaction();
+        //Adicionamos o container e o objeto do nosso fragmentoA para exibir.
+        transaction.add(R.id.frameLayout,new FragmentoA());
+        //Finalizamos a transação do fragment com commit.
+        transaction.commit();
+
+        buttonA.setOnClickListener(v -> {
+             FragmentTransaction ft =fragmentManager.beginTransaction();
+             ft.replace(R.id.frameLayout,new FragmentoA());
+             ft.commit();
         });
+        buttonB.setOnClickListener(v -> {
+            FragmentTransaction ft =fragmentManager.beginTransaction();
+            ft.replace(R.id.frameLayout,new FragmentoB());
+            ft.commit();
+        });
+        buttonC.setOnClickListener(v -> {
+            FragmentTransaction ft =fragmentManager.beginTransaction();
+            ft.replace(R.id.frameLayout,new FragmentoC());
+            ft.commit();
+        });
+
     }
+
+
 }
